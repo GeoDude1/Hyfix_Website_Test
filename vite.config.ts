b20a5f71c23fcbe3ae40eef15c2ae6b105e83fa2
GeoDude1 <legitgeo@gmail.com>
2026-02-17 16:15:02 -0800

    Set base for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { defineConfig } from "vite";
 export default defineConfig(({ mode }) => ({
   plugins: [react(), mode === "development" && screenGraphPlugin()],
   publicDir: "./static",
-  base: "./",
+  base: "/Hyfix_Website_Test/",
   css: {
     postcss: {
       plugins: [tailwind()],
